f1d85bb7e89904e2e8a565675b37ec96a8a0bd48
Brian Baldino <bbaldino@gmail.com>
2019-02-20 13:40:49 -0800

    update retransmissionrequester

--- src/main/kotlin/org/jitsi/nlj/rtcp/RetransmissionRequester.kt
+++ src/main/kotlin/org/jitsi/nlj/rtcp/RetransmissionRequester.kt
@@ -164,7 +164,7 @@ class RetransmissionRequester(
             logger.cdebug { "$ssrc doing work at ${clock.instant()}" }
             val now = clock.instant()
             val missingSeqNums = getMissingSeqNums()
-            val nackPacket = RtcpFbNackPacket.fromFields(mediaSourceSsrc = ssrc.toInt(), missingSeqNums = missingSeqNums)
+            val nackPacket = RtcpFbNackPacket.fromFields(mediaSourceSsrc = ssrc, missingSeqNums = missingSeqNums)
             notifyNackSent(now, missingSeqNums)
             rtcpSender(nackPacket)
         }
