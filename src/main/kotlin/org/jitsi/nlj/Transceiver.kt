9d2d1b4de06123b5008848c08200b7cc18f88035
George Politis <gp@jitsi.org>
2019-05-13 11:05:09 +0100

    adds time series logging

--- src/main/kotlin/org/jitsi/nlj/Transceiver.kt
+++ src/main/kotlin/org/jitsi/nlj/Transceiver.kt
@@ -94,7 +94,8 @@ class Transceiver(
             rtcpEventNotifier,
             senderExecutor,
             backgroundExecutor,
-            logLevelDelegate
+            logLevelDelegate,
+            diagnosticContext
     )
     private val rtpReceiver: RtpReceiver =
         RtpReceiverImpl(
