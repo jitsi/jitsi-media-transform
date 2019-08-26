a96f4b9f76b460d034c262f617281afac631ff63
Jonathan Lennox <jonathan.lennox@8x8.com>
2019-08-26 14:31:20 -0400

    Turn off SRTP sender replay protection.
    This is necessary in order to support RetransmissionSender.retransmitPlain.

--- src/main/kotlin/org/jitsi/nlj/srtp/SrtpUtil.kt
+++ src/main/kotlin/org/jitsi/nlj/srtp/SrtpUtil.kt
@@ -141,6 +141,10 @@ class SrtpUtil {
                 srtpProfileInformation.cipherSaltLength
             )
 
+            /* To support RetransmissionSender.retransmitPlain, we need to allow send-side SRTP replay. */
+            /* TODO: enable this only in cases where we actually need to use retransmitPlain? */
+            srtpPolicy.isSendReplayEnabled = true
+
             val clientSrtpContextFactory = SrtpContextFactory(
                 tlsRole == TlsRole.CLIENT,
                 clientWriteSrtpMasterKey,
