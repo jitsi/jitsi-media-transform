2f086ab10cde9a22e804ffe57091f1a8a5eb6bff
Jonathan Lennox <jonathan.lennox@8x8.com>
2019-09-05 17:37:02 -0400

    Initial sketch of abstract bandwidth estimator interface.

---
+++ src/main/kotlin/org/jitsi/nlj/rtp/bandwidthestimation/BandwidthEstimator.kt
@@ -0,0 +1,91 @@
+/*
+ * Copyright @ 2019 - present 8x8, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jitsi.nlj.rtp.bandwidthestimation
+
+import java.time.Instant
+
+/**
+ * An abstract interface to a bandwidth estimation algorithm.
+ *
+ * The invoker of the algorithm will periodically call [processPacketArrival]
+ * and/or [processPacketLoss] as it learns information about packets
+ * that have traversed the network.
+ *
+ * All bandwidths/bitrates are in bits per second.
+ */
+interface BandwidthEstimator {
+    /** The name of the algorithm implemented by this [BandwidthEstimator]. */
+    val algorithmName : String
+
+    /** The initial bandwidth estimate. */
+    var initBw: Float
+
+    /** The minimum bandwidth the estimator will return. */
+    var minBw: Float
+
+    /** The maximum bandwidth the estimator will return. */
+    var maxBw: Float
+
+    /**
+     * Inform the bandwidth estimator about a packet that has arrived at its
+     * destination.
+     *
+     * This function will be called at most once for any value of [seq];
+     * however, it may be called after a call to [processPacketLoss] for the
+     * same [seq] value, if a packet is delayed.
+     *
+     * It is possible (e.g., if feedback was lost) that neither
+     * [processPacketArrival] nor [processPacketLoss] is called for a given [seq].
+     *
+     * The clocks reported by [now], [sendTime], and [recvTime] do not
+     * necessarily have any relationship to each other, but must be consistent
+     * within themselves across all calls to functions of this [BandwidthEstimator].
+     *
+     * @param[now] The current time, when this function is called.
+     * @param[sendTime] The time the packet was sent, if known, or null.
+     * @param[recvTime] The time the packet was received, if known, or null.
+     * @param[seq] A 16-bit sequence number of packets processed by this
+     *  [BandwidthEstimator].
+     * @param[size] The size of the packet, in bytes.
+     * @param[ecn] The ECN markings with which the packet was received.
+     */
+    fun processPacketArrival(now: Instant,
+                             sendTime: Instant?,
+                             recvTime: Instant?,
+                             seq: Int,
+                             size: Int,
+                             ecn: Byte = 0)
+
+    /**
+     * Inform the bandwidth estimator that a packet was lost.
+     *
+     * @param[now] The current time, when this function is called.
+     * @param[sendTime] The time the packet was sent, if known, or null.
+     * @param[seq] A 16-bit sequence number of packets processed by this
+     *  [BandwidthEstimator].
+     */
+    fun processPacketLoss(now: Instant, sendTime: Instant?, seq: Int)
+
+    /** Get the estimator's current estimate of the available bandwidth.
+     *
+     * @param[now] The current time, when this function is called.
+     */
+    fun getCurrentBw(now: Instant): Float
+
+    /** Reset the estimator to its initial state. */
+    fun reset(): Unit
+}
