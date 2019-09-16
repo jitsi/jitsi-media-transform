2b19ddb18b18e753e11b33637e9b9afb34a6fdac
Jonathan Lennox <jonathan.lennox@8x8.com>
2019-09-16 14:06:01 -0400

    Do FakeScheduledExecutorService math in nanos rather than millis.
    Otherwise we can get stuck if the next task is < 1ms in the future.

--- src/test/kotlin/org/jitsi/nlj/test_utils/FakeScheduledExecutorService.kt
+++ src/test/kotlin/org/jitsi/nlj/test_utils/FakeScheduledExecutorService.kt
@@ -33,9 +33,9 @@ internal abstract class FakeScheduledExecutorService : ScheduledExecutorService 
     }
 
     override fun schedule(command: Runnable, delay: Long, unit: TimeUnit): ScheduledFuture<*> {
-        println("scheduling job with a delay of ${unit.toMillis(delay)} from time ${clock.instant()}")
+        println("scheduling job with a delay of $delay $unit from time ${clock.instant()}")
         val future: ScheduledFuture<Unit> = mock()
-        val nextRunTime = clock.instant().plus(Duration.ofMillis(unit.toMillis(delay)))
+        val nextRunTime = clock.instant().plus(Duration.ofNanos(unit.toNanos(delay)))
         val job = Job(command, nextRunTime)
         jobs.add(job)
 
