14d8638a8aff99314cccf12271a67bd2716c2928
Jonathan Lennox <jonathan.lennox@8x8.com>
2019-10-08 14:26:34 -0400

    Use TimeUtils.formatTimeAsFullMillis() for Instant and Duration formatMilli() add-on.

--- src/main/kotlin/org/jitsi/nlj/util/ClockUtils.kt
+++ src/main/kotlin/org/jitsi/nlj/util/ClockUtils.kt
@@ -18,63 +18,13 @@
 
 package org.jitsi.nlj.util
 
-import java.text.DecimalFormat
 import java.time.Duration
 import java.time.Instant
+import org.jitsi.utils.TimeUtils
 
 @JvmField
 val NEVER: Instant = Instant.MIN
 
-/** Get nanoseconds since the epoch of an Instant.
- *
- * Long can represent nanoseconds-since-the-epoch up through April 2262.
- * If you are still using this code after the year 2262 I apologize.
- */
-val Instant.epochNano: Long
-    get() {
-        val sec = this.epochSecond
-        val nano = this.nano
+fun Instant.formatMilli(): String = TimeUtils.formatTimeAsFullMillis(this.epochSecond, this.nano)
 
-        return sec * 1_000_000_000 + nano
-    }
-
-/** Get total nanoseconds of a Duration.
- *
- * Long nanoseconds can represent durations of up to 292 years.
- * If you need durations longer than that please use a different API.
- */
-val Duration.totalNanos: Long
-    get() {
-        val sec = this.seconds
-        val nano = this.nano
-
-        return sec * 1_000_000_000 + nano
-    }
-
-private val millionthsFormatter by lazy(LazyThreadSafetyMode.PUBLICATION) {
-    DecimalFormat(".######")
-}
-
-private fun formatMillionths(value: Long): String {
-    val neg = value < 0
-    val absValue = if (neg) -value else value
-
-    var ret = StringBuilder()
-    if (neg) {
-        ret.append("-")
-    }
-
-    val integer = absValue / 1_000_000
-    val millionths = absValue % 1_000_000
-
-    ret.append(integer.toString())
-
-    if (millionths != 0L)
-        ret.append(millionthsFormatter.format(millionths / 1e6))
-
-    return ret.toString()
-}
-
-fun Instant.formatMilli(): String = formatMillionths(this.epochNano)
-
-fun Duration.formatMilli(): String = formatMillionths(this.totalNanos)
+fun Duration.formatMilli(): String = TimeUtils.formatTimeAsFullMillis(this.seconds, this.nano)
