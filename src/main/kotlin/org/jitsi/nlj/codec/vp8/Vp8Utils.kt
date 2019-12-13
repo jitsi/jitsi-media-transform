352ac844d376cbd17e30de7a010f820bf3b097ad
Jonathan Lennox <jonathan.lennox@8x8.com>
2019-12-13 13:38:51 -0500

    Add utility functions to manipulate VP8 pic id and tl0 pic idx values.

--- src/main/kotlin/org/jitsi/nlj/codec/vp8/Vp8Utils.kt
+++ src/main/kotlin/org/jitsi/nlj/codec/vp8/Vp8Utils.kt
@@ -80,5 +80,63 @@ class Vp8Utils {
         fun getTemporalLayerIdOfFrame(vp8Packet: RtpPacket) =
             DePacketizer.VP8PayloadDescriptor.getTemporalLayerIndex(
                 vp8Packet.buffer, vp8Packet.payloadOffset, vp8Packet.payloadLength)
+
+        /**
+         * Returns the delta between two VP8 extended picture IDs, taking into account
+         * rollover.  This will return the 'shortest' delta between the two
+         * picture IDs in the form of the number you'd add to b to get a. e.g.:
+         * getPictureIdDelta(1, 10) -> -9 (10 + -9 = 1)
+         * getPictureIdDelta(1, 32760) -> 9 (32760 + 9 = 1)
+         * @return the delta between two extended picture IDs (modulo 2^15).
+         */
+        @JvmStatic
+        fun getPictureIdDelta(a: Int, b: Int): Int {
+            val diff = a - b
+            return when {
+                diff < -(1 shl 14) -> diff + (1 shl 15)
+                diff > (1 shl 14) -> diff - (1 shl 15)
+                else -> diff
+            }
+        }
+
+        /**
+         * Apply a delta to a given picture ID and return the result (taking
+         * rollover into account)
+         * @param start the starting picture ID
+         * @param delta the delta to be applied
+         * @return the picture ID resulting from doing "start + delta"
+         */
+        @JvmStatic
+        fun applyPictureIdDelta(start: Int, delta: Int): Int =
+            (start + delta) and DePacketizer.VP8PayloadDescriptor.EXTENDED_PICTURE_ID_MASK
+
+        /**
+         * Returns the delta between two VP8 Tl0PicIdx values, taking into account
+         * rollover.  This will return the 'shortest' delta between the two
+         * picture IDs in the form of the number you'd add to b to get a. e.g.:
+         * getPictureIdDelta(1, 10) -> -9 (10 + -9 = 1)
+         * getPictureIdDelta(1, 250) -> 7 (250 + 7 = 1)
+         * @return the delta between two extended picture IDs (modulo 2^8).
+         */
+        @JvmStatic
+        fun getTl0PicIdxDelta(a: Int, b: Int): Int {
+            val diff = a - b
+            return when {
+                diff < -(1 shl 7) -> diff + (1 shl 8)
+                diff > (1 shl 7) -> diff - (1 shl 8)
+                else -> diff
+            }
+        }
+
+        /**
+         * Apply a delta to a given Tl0PidIcx and return the result (taking
+         * rollover into account)
+         * @param start the starting Tl0PicIdx
+         * @param delta the delta to be applied
+         * @return the Tl0PicIdx resulting from doing "start + delta"
+         */
+        @JvmStatic
+        fun applyTl0PicIdxDelta(start: Int, delta: Int): Int =
+            (start + delta) and DePacketizer.VP8PayloadDescriptor.TL0PICIDX_MASK
     }
 }
