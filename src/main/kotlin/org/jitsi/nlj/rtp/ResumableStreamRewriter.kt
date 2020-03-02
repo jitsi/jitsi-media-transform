04ed353d52352f6c03a068bb791f3960c28c4793
Jonathan Lennox <jonathan.lennox@8x8.com>
2020-03-02 17:22:46 -0500

    Add a mode to ResumableStreamRewriter that lets us keep sequence number history.

--- src/main/kotlin/org/jitsi/nlj/rtp/ResumableStreamRewriter.kt
+++ src/main/kotlin/org/jitsi/nlj/rtp/ResumableStreamRewriter.kt
@@ -15,6 +15,8 @@
  */
 package org.jitsi.nlj.rtp
 
+import org.jitsi.nlj.util.ArrayCache
+import org.jitsi.nlj.util.Rfc3711IndexTracker
 import org.jitsi.rtp.rtp.RtpPacket
 import org.jitsi.rtp.util.isNewerThan
 
@@ -28,14 +30,17 @@ import org.jitsi.rtp.util.isNewerThan
  * @author Maryam Daneshi
  * @author George Politis
  * @author Boris Grozev
+ * @author Jonathan Lennox
  */
-class ResumableStreamRewriter() {
+class ResumableStreamRewriter(val keepHistory: Boolean = false) {
     /**
      * The sequence number delta between what's been accepted and what's been
      * received, mod 2^16.
      */
     var seqnumDelta = 0
         private set
+
+    private var history: StreamRewriteHistory? = null
 
     /**
      * The highest sequence number that got accepted, mod 2^16.
@@ -65,6 +70,15 @@ class ResumableStreamRewriter() {
      * @return a rewritten sequence number that hides any gaps caused by drops.
      */
     fun rewriteSequenceNumber(accept: Boolean, sequenceNumber: Int): Int {
+        if (keepHistory && !accept && history == null) {
+            /* Don't instantiate history until it's needed; many streams never discard. */
+            history = StreamRewriteHistory()
+        }
+
+        history?.let {
+            return it.rewriteSequenceNumber(accept, sequenceNumber)
+        }
+
         if (accept) {
             // overwrite the sequence number (if needed)
             val newSequenceNumber = (sequenceNumber - seqnumDelta) and 0xffff
@@ -86,6 +100,147 @@ class ResumableStreamRewriter() {
             }
 
             return sequenceNumber
+        }
+    }
+
+    val gapsLeft = history?.gapsLeft ?: 0
+
+    private class RewriteHistoryItem(
+        var accept: Boolean?,
+        val newIndex: Int
+    )
+
+    private class StreamRewriteHistory : ArrayCache<RewriteHistoryItem>(
+        MAX_REWRITE_HISTORY,
+        /* We don't want to clone objects that get put in the tracker. */
+        { it },
+        /* Caller should have this object synchronized if needed. */
+        synchronize = false
+        ) {
+        var firstIndex: Int = -1
+
+        var gapsLeft = 0
+            private set
+
+        private val rfc3711IndexTracker = Rfc3711IndexTracker()
+
+        private fun fillBetween(start: Int, end: Int, firstNewIndex: Int) {
+            if (end <= lastIndex - size + 1)
+                return
+            val actualStart = if (start <= lastIndex - size) {
+                lastIndex - size + 1
+            } else {
+                start
+            }
+
+            var newIndex = firstNewIndex
+
+            for (i in actualStart..end) {
+                insertItem(RewriteHistoryItem(null, newIndex), i)
+                newIndex++
+            }
+        }
+
+        fun rewriteSequenceNumber(accept: Boolean, sequenceNumber: Int): Int {
+            val index = rfc3711IndexTracker.interpret(sequenceNumber)
+
+            val newIndex: Int
+
+            when {
+                (firstIndex == -1) -> {
+                    /* First index seen. */
+                    insertItem(RewriteHistoryItem(accept, index), index)
+
+                    firstIndex = index
+
+                    newIndex = index
+                }
+
+                (index > lastIndex) -> {
+                    /* New.  Roll forward, filling in gap if necessary. */
+
+                    val newestIndex = lastIndex
+
+                    val newest = getContainer(newestIndex)
+                        ?: throw IllegalStateException("No newest container found")
+
+                    val newestNewIndex = newest.item!!.newIndex
+
+                    val indexGap = index - newestIndex
+
+                    val newGap = indexGap - if (accept) 0 else 1
+
+                    newIndex = newestNewIndex + newGap
+
+                    insertItem(RewriteHistoryItem(accept, newIndex), index)
+
+                    fillBetween(newestIndex + 1, index - 1, newestNewIndex + 1)
+                }
+
+                (index > lastIndex - size && index >= firstIndex) -> {
+                    /* In history.  Retrieve. */
+
+                    val container = getContainer(index)
+                        ?: throw IllegalStateException("No container found for index $index in history")
+                    val item = container.item!!
+
+                    if (item.accept == null) {
+                        item.accept = accept
+                        if (!accept)
+                            gapsLeft++
+                    }
+
+                    newIndex = item.newIndex
+                }
+
+                (index > lastIndex - size && index < firstIndex) -> {
+                    /* Older than previous oldest, but still in range of the map.
+                       Project map backwards. */
+
+                    val oldestIndex = Math.max(index - size, firstIndex)
+                    val oldest = getContainer(oldestIndex)
+                        ?: throw IllegalStateException("No oldest container found")
+
+                    val oldestNewIndex = oldest.item!!.newIndex
+
+                    val indexGap = index - oldestIndex /* Negative */
+
+                    val newGap = indexGap + if (accept) 0 else 1
+
+                    newIndex = oldestNewIndex + newGap
+
+                    insertItem(RewriteHistoryItem(accept, newIndex), index)
+
+                    fillBetween(index + 1, oldestIndex - 1, newIndex + 1)
+
+                    firstIndex = index
+                }
+
+                else -> {
+                    /* Older than the map. */
+
+                    val oldestIndex = Math.max(index - size, firstIndex)
+                    val oldest = getContainer(oldestIndex)
+                        ?: throw IllegalStateException("No oldest container found")
+                    val oldestDelta = oldestIndex - oldest.item!!.newIndex
+
+                    newIndex = index - oldestDelta
+                }
+            }
+
+            if (accept) return newIndex and 0xffff
+            return sequenceNumber /* Don't care about sequence numbers for non-accepted packets,
+              so make sure rewriteRtp does nothing. */
+        }
+
+        companion object {
+            /**
+             * The maximum number of received packets and their timestamps to save.
+             *
+             * NOTE rtt + minimum amount
+             * XXX this is an uninformed value.
+             */
+            private const val MAX_REWRITE_HISTORY = 1000
         }
     }
 }
